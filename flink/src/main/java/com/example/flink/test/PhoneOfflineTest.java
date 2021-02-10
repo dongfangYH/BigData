0589df6e507e58527aacd3045109498e883bd7dd
yuanhang.liu <yuanhang.liu@tcl.com>
2021-02-10 09:45:13 +0800

    add attachment

--- flink/src/main/java/com/example/flink/test/PhoneOfflineTest.java
+++ flink/src/main/java/com/example/flink/test/PhoneOfflineTest.java
@@ -6,6 +6,7 @@ import com.example.flink.source.PhoneEventGenerator;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.flink.api.common.eventtime.*;
 import org.apache.flink.api.common.restartstrategy.RestartStrategies;
+import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -27,6 +28,7 @@ public class PhoneOfflineTest {
         TimeZone.setDefault(TimeZone.getTimeZone("GMT+8"));
         final StreamExecutionEnvironment streamEnv = StreamExecutionEnvironment.getExecutionEnvironment();
         streamEnv.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+        streamEnv.enableCheckpointing(60000L, CheckpointingMode.EXACTLY_ONCE);
         //streamEnv.setParallelism(1);
         /*streamEnv.setRestartStrategy(RestartStrategies.fixedDelayRestart(
                 1,
